39e76b090c4eae220b591b6eeedb79d13793b18a
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-06-27 16:03:23 +0500

    start

---
+++ data/config.py
@@ -0,0 +1,8 @@
+
+
+""" Команды alembic
+alembic init alembic -- инициализация
+alembic revision --autogenerate -m "Коментарии"
+alembic upgrade head - upgrade  --выполняет код для изменения состояния базы
+                       downgrade -- код для возврата к предыдущему состоянию
+"""
--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-""" Команды alembic
-alembic init alembic -- инициализация
-alembic revision --autogenerate -m "Коментарии"
-alembic upgrade head - upgrade  --выполняет код для изменения состояния базы
-                       downgrade -- код для возврата к предыдущему состоянию
-"""
-
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index.html', methods=['GET', 'POST'])
