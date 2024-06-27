824cee69ed288138f700bfa7012b02122911a642
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-06-27 15:39:57 +0500

    start

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+""" Команды alembic
+alembic init alembic -- инициализация
+alembic revision --autogenerate -m "Коментарии"
+alembic upgrade head - upgrade  --выполняет код для изменения состояния базы
+                       downgrade -- код для возврата к предыдущему состоянию
+"""
+
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
