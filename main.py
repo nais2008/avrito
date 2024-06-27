b0daeb29b54bd5213214ddab33cb97ab822f5ea9
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-06-27 23:42:42 +0500

    start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import render_template, url_for, redirect, abort, request, jsonify, make_response
+from config import *
 
 
 @app.route('/', methods=['GET', 'POST'])
